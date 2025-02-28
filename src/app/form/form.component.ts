088c5cf8e799a851f34ce0bd27602b651b5fde56
Tigaas <trr08469@gmail.com>
2025-02-28 16:00:09 -0300

    atualização aula 2802

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -7,5 +7,33 @@ import { Component } from '@angular/core';
   styleUrl: './form.component.css'
 })
 export class FormComponent {
+  num1: number = 0;
+  num2: number = 0;
+  sum: number = 0;
+
+  
+
+  onClicksum(){
+    this.sum = this.num1 + this.num2;
+  }
+  op1(){
+    this.sum = this.num1 - this.num2;
+  }
+  op2(){
+    this.sum = this.num1 * this.num2;
+  }
+  op3(){
+    this.sum = this.num1 / this.num2;
+  }
+  op4(){
+     this.num1 = 0 ;
+      this.num2 = 0;
+      this.sum = 0;
+  }
+  op5(){
+    this.sum = this.num1 ** this.num2
+    ;
+  }
+
 
 }
